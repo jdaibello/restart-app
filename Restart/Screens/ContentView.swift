33bdcc209947efcd24266eb450144ba07d617e1e
João Pedro <jpedrodaibello1999@outlook.com>
2023-02-28 17:02:11 -0300

    feat: add Onboarding Screen

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("onboarding") var isOrboardingViewActive: Bool = true
-    
-    var body: some View {
-        ZStack {
-            if isOrboardingViewActive {
-                OnboardingView()
-            } else {
-                HomeView()
-            }
-        }
+  @AppStorage("onboarding") var isOrboardingViewActive: Bool = true
+  
+  var body: some View {
+    ZStack {
+      if isOrboardingViewActive {
+        OnboardingView()
+      } else {
+        HomeView()
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -8,25 +8,25 @@
 import SwiftUI
 
 struct HomeView: View {
-    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
-    
-    var body: some View {
-        VStack(spacing: 20) {
-            Text("Home").font(.largeTitle)
-            Button(
-                action: {
-                    isOnboardingViewActive = true
-                },
-                label: {
-                    Text("Restart")
-                }
-            )
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+  
+  var body: some View {
+    VStack(spacing: 20) {
+      Text("Home").font(.largeTitle)
+      Button(
+        action: {
+          isOnboardingViewActive = true
+        },
+        label: {
+          Text("Restart")
         }
+      )
     }
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+  static var previews: some View {
+    HomeView()
+  }
 }
