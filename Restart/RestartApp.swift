49ec1487292ed31d6eaebc988adfe9726dc84b57
João Pedro <jpedrodaibello1999@outlook.com>
2023-02-28 11:39:52 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by João Pedro Daibello on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
